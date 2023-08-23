40a5c48b116d62095e4fbfbef5f22a7dc66d421b
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2023-08-23 16:18:04 -0300

    feat: reusability with props

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -12,43 +12,57 @@ const starContainerStyle = {
   display: 'flex',
 };
 
-const textStyle = {
-  lineHeight: '1',
-  margin: '0'
-};
 
-export const StarRating = ({maxRating = 5}) => {
-
+export const StarRating = ({
+  maxRating = 5,
+  color = '#fcc419',
+  size = 24,
+  className = '',
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) => {
+  
   //states
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
-
+  
   //functions
   function handleSetRating(rating) {
     setRating(rating);
+    onSetRating(rating);
   }
-
+  
   function handleMouseIn(index) {
     setTempRating(index + 1);
   }
-
+  
   function handleMouseOut() {
     setTempRating(0);
   }
-
-
+  
+  //styles
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    color,
+    fontSize: `${size}px`,
+  };
+  
+  
   return (
-    <div style={containerStyle}>
+    <div className={className} style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({length: maxRating}, (_, i) => {
-          return <Start key={i}
+          return <Start key={i} color={color} size={size}
                         full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
                         onRate={() => handleSetRating(i + 1)}
                         onHoverIn={() => handleMouseIn(i)}
                         onHoverOut={() => handleMouseOut()}/>;
         })}
       </div>
-      <p style={textStyle}>{tempRating || rating || ''}</p>
+      <p
+        style={textStyle}>{messages.length === maxRating ? messages[tempRating ? tempRating - 1 : rating - 1] : tempRating || rating || ''}</p>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import {StarRating} from './components/StarRating';
 // import './index.css';
@@ -8,6 +8,23 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
-    <StarRating maxRating={5}/>
+    <StarRating maxRating={5} defaultRating={1}
+                messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/>
+    <StarRating className="test" maxRating={10} color="orange" size={32}
+                defaultRating={3}/>
+    <Test/>
   </React.StrictMode>
 );
+
+
+function Test() {
+  
+  const [rating, setRating] = useState(0);
+  
+  return (
+    <div>
+      <StarRating onSetRating={setRating}/>
+      <p>This movie received {rating} stars from the critics. </p>
+    </div>
+  );
+}
