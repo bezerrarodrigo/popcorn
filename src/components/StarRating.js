e127db49c44bc9129ddc9aac97727ef5d1fb8f30
Rodrigo Bezerra <bezerra.rodrigo@gmail.com>
2023-08-22 23:48:14 -0300

    feat: reusable star rating component

---
+++ src/components/StarRating.js
@@ -0,0 +1,31 @@
+//styles
+const containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '16px'
+};
+
+const starContainerStyle = {
+  display: 'flex',
+  gap: '4px'
+};
+
+const textStyle = {
+  lineHeight: '1',
+  margin: '0'
+};
+
+export const StarRating = ({maxRating = 5}) => {
+
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({length: maxRating}, (_, i) => {
+          return <span>S{i + 1}</span>;
+        })}
+      </div>
+      <p style={textStyle}>10</p>
+    </div>
+  );
+};
+
